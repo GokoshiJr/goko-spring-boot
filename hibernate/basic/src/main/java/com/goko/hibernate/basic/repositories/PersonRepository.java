ab634d8cb5c6a9a451132e22f1004f54d8cd545f
GokoshiJr <julioagonzalez18@gmail.com>
2024-04-05 15:06:40 -0400

    fix [hibernate]: update HomeController

--- hibernate/basic/src/main/java/com/goko/hibernate/basic/repositories/PersonRepository.java
+++ hibernate/basic/src/main/java/com/goko/hibernate/basic/repositories/PersonRepository.java
@@ -5,8 +5,12 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.goko.hibernate.basic.entities.Person;
 import java.util.List;
+import java.util.Optional;
 
 public interface PersonRepository extends CrudRepository<Person, Long> {
+    @Query(value = "SELECT * FROM Persons WHERE name LIKE %?1% LIMIT 1", nativeQuery = true)
+    Optional<Person> findOneLikeName(String name);
+
     List<Person> findByHomeDirection(String homeDirection);
 
     @Query("SELECT p FROM Person p WHERE homeDirection LIKE %?1%")
@@ -14,4 +18,5 @@ public interface PersonRepository extends CrudRepository<Person, Long> {
 
     @Query("SELECT p.name, p.homeDirection FROM Person p WHERE p.id = %?1%")
     List<Object[]> findHomeDirectionByPersonId(Long Id);
+
 }
