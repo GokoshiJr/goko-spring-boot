3708ee3659d2d8c6c57c8566f293d6763b6c1adf
julio <julioagonzalez18@gmail.com>
2024-05-12 14:04:48 -0400

    fix[hibernate]: Entity life cycle
    - Prepersist
    - Preupdate
    
    Update README.md

--- hibernate/README.md
+++ hibernate/README.md
@@ -1,1 +1,5 @@
 curl -X POST localhost:8080/api/persons -d "{\"name\":\"Andres\",\"lastName\":\"Guzman\",\"age\":40,\"homeDirection\":\"SD\"}" -H "Content-Type: application/json"
+
+curl localhost:8080/api/persons/1 | python -m json.tool
+
+psql -d dbName -U user -W password
--- hibernate/basic/src/main/java/com/goko/hibernate/basic/entities/Person.java
+++ hibernate/basic/src/main/java/com/goko/hibernate/basic/entities/Person.java
@@ -1,10 +1,14 @@
 package com.goko.hibernate.basic.entities;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,6 +20,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Person {
+    @PrePersist
+    public void prePersist() {
+        System.out.println("Evento de ciclo de vida pre persist");
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = this.createdAt;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        System.out.println("Evento de ciclo de vida pre update");
+        this.updatedAt = LocalDateTime.now();
+    }
 
     Person(String name, String homeDirection) {
         this.name = name;
@@ -31,6 +47,10 @@ public class Person {
     private String lastName;
     
     private int age;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
     
     @Column(name = "home_direction")
     private String homeDirection;
